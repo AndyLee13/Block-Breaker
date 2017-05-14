6ea91217869399f130711c8ef7032925eb29eca5
Chengcheng Li <lcc199533@gmail.com>
2017-05-14 11:47:59 +0800

    Initial Commit
    Lay down the foundation of the game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	private Vector3 paddleToBallVector; 
+	private bool hasStarted = false;
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			// Lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		}
+
+		//wait for a mouse click to launch
+		if (Input.GetMouseButtonDown(0)) {
+			hasStarted = true;
+			this.rigidbody2D.velocity = new Vector2(2f,10f);
+		}
+	}
+}
